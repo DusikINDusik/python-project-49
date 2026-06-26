76887a58eb46f322fb3dcd354e1c4ebc1d2b3663
Ed <dusikindusik@gmail.com>
2026-06-26 16:55:09 +0500

    my first project from Hexlet

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,7 +2,7 @@ from brain_games.engine import games_engine
 from brain_games.games.prime import is_prime
 
 def main():
-    games_engine(is_prime, 'Answer "yes" if given number is prime. Otherwise answer "no"')
+    games_engine(is_prime, 'Answer "yes" if given number is prime. Otherwise answer "no".')
 
 if __name__ == '__main__':
     main()
